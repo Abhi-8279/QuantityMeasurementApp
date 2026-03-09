583861f4b60d3968a87db4c95c6dd4c9cda24924
Abhishek Gupta <abhishek.gupta_cs22@gla.ac.in>
2026-03-09 10:57:22 +0530

    Added UC-9

---
+++ src/main/java/com/quantityApp/LengthUnit.java
@@ -0,0 +1,27 @@
+package com.quantityApp;
+
+public enum LengthUnit {
+
+    FEET(1.0),
+    INCHES(1.0 / 12.0),
+    YARDS(3.0),
+    CENTIMETERS(0.0328084);
+
+    private final double conversionFactorToFeet;
+
+    LengthUnit(double conversionFactorToFeet) {
+        this.conversionFactorToFeet = conversionFactorToFeet;
+    }
+
+    public double convertToBaseUnit(double value) {
+        return value * conversionFactorToFeet;
+    }
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / conversionFactorToFeet;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactorToFeet;
+    }
+}
