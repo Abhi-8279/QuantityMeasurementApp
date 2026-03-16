389ed698c18d49c60735e9e2bdc2e10a0df8a774
Abhishek Gupta <abhishek.gupta_cs22@gla.ac.in>
2026-03-16 11:16:51 +0530

    Added feature-UC13

---
+++ src/main/java/com/quantityApp/QuantityMeasurementApp.java
@@ -0,0 +1,15 @@
+package com.quantityApp;
+
+public class QuantityMeasurementApp{
+	public static void main(String[] args) {
+		Quantity<LengthUnit> q1 =
+		        new Quantity<>(10.0, LengthUnit.FEET);
+
+		Quantity<LengthUnit> q2 =
+		        new Quantity<>(6.0, LengthUnit.INCHES);
+
+		System.out.println(q1.subtract(q2));
+		System.out.println(q1.add(q2));
+		System.out.println(q1.divide(q2));
+	}
+}
