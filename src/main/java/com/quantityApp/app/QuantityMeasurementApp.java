016d6145a8e896aa8ece1f423a45b6061def4919
Abhishek Gupta <abhishek.gupta_cs22@gla.ac.in>
2026-03-16 12:18:17 +0530

    [Abhishek]Added.UC-16

--- src/main/java/com/quantityApp/app/QuantityMeasurementApp.java
+++ src/main/java/com/quantityApp/app/QuantityMeasurementApp.java
@@ -1,29 +1,25 @@
 package com.quantityApp.app;
 
 import com.quantityApp.controller.QuantityMeasurementController;
-import com.quantityApp.dto.QuantityDTO;
-import com.quantityApp.repository.QuantityMeasurementCacheRepository;
+import com.quantityApp.repository.IQuantityMeasurementRepository;
+import com.quantityApp.repository.QuantityMeasurementDatabaseRepository;
+import com.quantityApp.service.IQuantityMeasurementService;
 import com.quantityApp.service.QuantityMeasurementServiceImpl;
 
 public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        QuantityMeasurementCacheRepository repository =
-                QuantityMeasurementCacheRepository.getInstance();
+    	IQuantityMeasurementRepository repository =
+    	        new QuantityMeasurementDatabaseRepository();
 
-        QuantityMeasurementServiceImpl service =
-                new QuantityMeasurementServiceImpl(repository);
+    	IQuantityMeasurementService service =
+    	        new QuantityMeasurementServiceImpl(repository);
 
-        QuantityMeasurementController controller =
-                new QuantityMeasurementController(service);
+    	QuantityMeasurementController controller =
+    	        new QuantityMeasurementController(service);
 
-        QuantityDTO q1 = new QuantityDTO(1.0, "FEET", "LENGTH");
-        QuantityDTO q2 = new QuantityDTO(12.0, "INCHES", "LENGTH");
-        QuantityDTO q3 = new QuantityDTO(24.0,"INCHES","LENGTH");
+    	controller.run();
 
-        controller.performComparison(q1, q2);
-        controller.performAddition(q1, q2);
-        controller.performComparison(q2, q3);
     }
 }
